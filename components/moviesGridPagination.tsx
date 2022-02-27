5f7e048ffdb51b1beee79d42b130245b5e6b2a87
ycruz-webimpacto <ycruz-webimpacto@webimpacto.es>
2022-02-27 10:08:50 -0500

    fix: fix spaces in pagination

--- components/moviesGridPagination.tsx
+++ components/moviesGridPagination.tsx
@@ -7,17 +7,17 @@ interface IProps {
 
 const MoviesGridPagination = ({ page, pages }: IProps) => {
 	return <div className='flex justify-around pt-8 pb-0'>
-		<div>
+		<div className="w-1/3 text-center">
 			{page > 1 &&
 				<Link href={page > 2 ? `/movies/${page - 1}` : `/`}>
 					<a className=''>&larr; prev</a>
 				</Link>
 			}
 		</div>
-		<div className='text-base'>
+		<div className='w-1/3 text-base text-center'>
 			Page {page}/{pages}
 		</div>
-		<div>
+		<div className="w-1/3 text-center">
 			{page < pages &&
 				<Link href={`/movies/${page + 1}`}>
 					<a className=''>next &rarr;</a>
